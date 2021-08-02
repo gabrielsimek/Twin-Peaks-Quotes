8865fb0b124de1196208e91e04e47cdd5407befb
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-02 16:35:13 -0700

    basic functionality working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Quote from '../quote/Quote';
 import Load from '../quote/Load';
+import TwinPeaksQuote from '../../containers/TwinPeaksQuote';
 
 export default function App() {
   return (
-    <> 
-      <Load/> 
-      <Quote/>
-    </>
+    <TwinPeaksQuote/>
   );
  
 }
