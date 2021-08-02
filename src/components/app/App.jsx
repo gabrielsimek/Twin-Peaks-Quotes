636dfd97a9a31be65576ec47664a52dd0f0fecfc
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-02 16:25:30 -0700

    Button component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Quote from '../quote/Quote';
+import Load from '../quote/Load';
 
 export default function App() {
-  return <Quote/>;
+  return (
+    <> 
+      <Load/> 
+      <Quote/>
+    </>
+  );
+ 
 }
