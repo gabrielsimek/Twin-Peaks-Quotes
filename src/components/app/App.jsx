3dbda0b79fa857d20cb7bd2832a8705fb4f2b863
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-03 09:47:51 -0700

    update snapshot

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Quote from '../quote/Quote';
-import Load from '../quote/Load';
 import TwinPeaksQuote from '../../containers/TwinPeaksQuote';
 
 export default function App() {
